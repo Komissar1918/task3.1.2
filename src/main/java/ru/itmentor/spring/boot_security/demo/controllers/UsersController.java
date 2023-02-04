77897a85fa8fc7a16d4f7996910d87548b474848
Timur Garipov <tr.garipov@yandex.ru>
2023-02-05 00:01:01 +0300

    init

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controllers/UsersController.java
@@ -0,0 +1,71 @@
+package ru.itmentor.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.itmentor.spring.boot_security.demo.models.User;
+import ru.itmentor.spring.boot_security.demo.services.UserService;
+
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String getUsers(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        model.addAttribute("user", userService.findOne(id));
+        return "show";
+    }
+
+    @GetMapping("/addNewUser")
+    public String addNewUser(Model model) {
+        model.addAttribute("user", new User());
+        return "userInfo";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        System.out.println("Новый user");
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.findOne(id));
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
+
+
+
+}
---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package ru.itmentor.spring.boot_security.demo.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.itmentor.spring.boot_security.demo.models.User;
+import ru.itmentor.spring.boot_security.demo.repositories.UserRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findOne(Integer id) {
+        Optional<User> findUser = userRepository.findById(id);
+        return findUser.orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+
+
+    @Override
+    @Transactional
+    public void update(Integer id, User user) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
